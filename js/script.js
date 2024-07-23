0969a29bb7a44a926508d2618da68a23703ad10e
mehdibenhabib <mehdimhb@hotmail.fr>
2024-07-23 19:45:46 +0100

    final update

--- js/script.js
+++ js/script.js
@@ -56,6 +56,7 @@ btnsRetirerAchat.forEach(btn => {
     btn.addEventListener('click', function() {
         let prixText = btn.previousElementSibling.previousElementSibling.textContent.trim();
         let prix = parseFloat(prixText.match(/[\d\.]+/)); 
+        panierTotalAchat -= prix;
         updatePanierTotal('achat');
         toggleRetirerButton(btn.nextElementSibling);
     });
@@ -74,3 +75,48 @@ btnsRetirerLocation.forEach(btn => {
         toggleRetirerButton(btn.nextElementSibling);
     });
 });
+
+
+// contact
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+  
+    var response = document.getElementById('response');
+    response.innerHTML = `Merci ${name}! Votre message "${message}" a été envoyé.`;
+    
+  });
+
+
+
+
+
+// login
+
+  function showCreateAccount() {
+    var loginForm = document.getElementById('login-form');
+    var createAccountForm = document.getElementById('create-account-form');
+    loginForm.style.animation = 'fadeOut 0.5s ease-out forwards';
+    createAccountForm.style.animation = 'fadeIn 0.5s ease-out forwards';
+    setTimeout(function() {
+        loginForm.style.display = 'none';
+        createAccountForm.style.display = 'block';
+    }, 500);
+}
+
+function showLoginForm() {
+    var loginForm = document.getElementById('login-form');
+    var createAccountForm = document.getElementById('create-account-form');
+    createAccountForm.style.animation = 'fadeOut 0.5s ease-out forwards';
+    loginForm.style.animation = 'fadeIn 0.5s ease-out forwards';
+    setTimeout(function() {
+        createAccountForm.style.display = 'none';
+        loginForm.style.display = 'block';
+    }, 500);
+}
+
+  
